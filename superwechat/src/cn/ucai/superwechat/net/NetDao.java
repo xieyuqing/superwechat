e9db9a6bf99a3e81d89f095060e6e3920a63d397
xieyuqing <451658995@qq.com>
2017-02-13 19:58:14 +0800

    更新用户的头像

--- superwechat/src/cn/ucai/superwechat/net/NetDao.java
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -4,6 +4,8 @@ import android.content.Context;
 
 import com.hyphenate.easeui.domain.User;
 
+import java.io.File;
+
 import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.utils.MD5;
 import cn.ucai.superwechat.utils.OkHttpUtils;
@@ -54,12 +56,24 @@ public class NetDao {
     }
 
     public static void updateUsernick(Context context, String username, String usernick,
-                                      OkHttpUtils.OnCompleteListener<String> listener){
+                                      OnCompleteListener<String> listener){
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
                 .addParam(I.User.USER_NAME,username)
                 .addParam(I.User.NICK,usernick)
                 .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void uploadUserAvatar(Context context, String username, File file,
+                                        OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID,username)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
                 .execute(listener);
     }
 }
--- superwechat/src/cn/ucai/superwechat/ui/UserProfileActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/UserProfileActivity.java
@@ -21,9 +21,15 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.utils.EaseImageUtils;
 import com.hyphenate.easeui.utils.EaseUserUtils;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -142,7 +148,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
 	private void updateRemoteNick(final String nickName) {
 		dialog = ProgressDialog.show(this, getString(R.string.dl_update_nick), getString(R.string.dl_waiting));
 		NetDao.updateUsernick(this, EMClient.getInstance().getCurrentUser(), nickName,
-				new OkHttpUtils.OnCompleteListener<String>() {
+				new OnCompleteListener<String>() {
 					@Override
 					public void onSuccess(String s) {
 						dialog.dismiss();
@@ -222,7 +228,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
 				break;
 			case REQUESTCODE_CUTTING:
 				if (data != null) {
-					setPicToView(data);
+					uploadAppUserAvatar(data);
 				}
 				break;
 			default:
@@ -258,6 +264,67 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
 			uploadUserAvatar(Bitmap2Bytes(photo));
 		}
 
+	}
+
+	private void uploadAppUserAvatar(Intent picdata) {
+		File file = saveBitmapFile(picdata);
+		L.e(TAG,"file="+file);
+		if (file == null) {
+			return;
+		}
+		L.e(TAG,"file="+file.getAbsolutePath());
+		dialog = ProgressDialog.show(this,getString(R.string.dl_update_photo),getString(R.string.dl_waiting));
+		NetDao.uploadUserAvatar(this, EMClient.getInstance().getCurrentUser(), file,
+				new OnCompleteListener<String>() {
+					@Override
+					public void onSuccess(String s) {
+						L.e(TAG,"s="+s);
+						if (s != null) {
+							Result result = ResultUtils.getResultFromJson(s, User.class);
+							if (result != null) {
+								if (result.isRetMsg()) {
+									User user = (User) result.getRetData();
+									if (user != null) {
+										PreferenceManager.getInstance().setCurrentUserAvatar(user.getAvatar());
+										SuperWeChatHelper.getInstance().saveAppContact(user);
+										EaseUserUtils.setAppUserAvatar(UserProfileActivity.this,
+												user.getMUserName(),mIvUserinfoAvatar);
+										CommonUtils.showShortToast(R.string.toast_updatephoto_success);
+									}
+								}
+							}
+						}
+						dialog.dismiss();
+					}
+
+					@Override
+					public void onError(String error) {
+						L.e(TAG,"error="+error);
+						dialog.dismiss();
+						CommonUtils.showShortToast(R.string.toast_updatephoto_fail);
+					}
+				});
+
+	}
+
+	private File saveBitmapFile(Intent picdata) {
+		Bundle extras = picdata.getExtras();
+		if (extras != null) {
+			Bitmap bitmap = extras.getParcelable("data");
+			String imagePath = EaseImageUtils.getImagePath(EMClient.getInstance().getCurrentUser()+I.AVATAR_SUFFIX_JPG);
+			File file = new File(imagePath);//将要保存图片的路径
+			L.e("file path="+file.getAbsolutePath());
+			try {
+				BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+				bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+				bos.flush();
+				bos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return file;
+		}
+		return null;
 	}
 
 	private void uploadUserAvatar(final byte[] data) {
