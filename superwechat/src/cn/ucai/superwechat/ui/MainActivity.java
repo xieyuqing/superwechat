acdba94e3ee657bf727763e2b7b5574b09e0a7e4
xieyuqing <451658995@qq.com>
2017-02-14 20:51:35 +0800

    MainActivity显示通讯录的未读消息标识

--- superwechat/src/cn/ucai/superwechat/ui/MainActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/MainActivity.java
@@ -206,7 +206,6 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
      */
     private void initView() {
 //		unreadLabel = (TextView) findViewById(R.id.unread_msg_number);
-//		unreadAddressLable = (TextView) findViewById(R.id.unread_address_number);
 //		mTabs = new Button[3];
 //		mTabs[0] = (Button) findViewById(R.id.btn_conversation);
 //		mTabs[1] = (Button) findViewById(R.id.btn_address_list);
@@ -460,16 +459,19 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
      * update the total unread count
      */
     public void updateUnreadAddressLable() {
-//		runOnUiThread(new Runnable() {
-//			public void run() {
-//				int count = getUnreadAddressCountTotal();
-//				if (count > 0) {
-//					unreadAddressLable.setVisibility(View.VISIBLE);
-//				} else {
-//					unreadAddressLable.setVisibility(View.INVISIBLE);
-//				}
-//			}
-//		});
+		runOnUiThread(new Runnable() {
+			public void run() {
+				int count = getUnreadAddressCountTotal();
+                L.e(TAG,"updateUnreadAddressLable,count="+count);
+				if (count > 1) {
+                    mLayoutTabhost.setUnreadCount(1,count);
+                } else if (count == 1) {
+                    mLayoutTabhost.setHasNew(1, true);
+                } else {
+                    mLayoutTabhost.setHasNew(1,false);
+                }
+            }
+		});
 
     }
 
