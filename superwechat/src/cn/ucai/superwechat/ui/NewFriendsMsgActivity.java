d8bbb280809a1ecd692d192dedd955d787f48de9
xieyuqing <451658995@qq.com>
2017-02-14 21:12:58 +0800

    申请与通知逻辑并显示数据

--- superwechat/src/cn/ucai/superwechat/ui/NewFriendsMsgActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/NewFriendsMsgActivity.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,20 @@ package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.adapter.NewFriendsMsgAdapter;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.domain.InviteMessage;
-
-import java.util.List;
+import cn.ucai.superwechat.utils.MFGT;
 
 /**
  * Application and notification
@@ -30,22 +36,45 @@ import java.util.List;
  */
 public class NewFriendsMsgActivity extends BaseActivity {
 
+	@BindView(R.id.img_back)
+	ImageView mImgBack;
+	@BindView(R.id.txt_title)
+	TextView mTxtTitle;
+	@BindView(R.id.txt_right)
+	TextView mTxtRight;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.em_activity_new_friends_msg);
+		ButterKnife.bind(this);
 
 		ListView listView = (ListView) findViewById(R.id.list);
 		InviteMessgeDao dao = new InviteMessgeDao(this);
 		List<InviteMessage> msgs = dao.getMessagesList();
 
-		NewFriendsMsgAdapter adapter = new NewFriendsMsgAdapter(this, 1, msgs); 
+		NewFriendsMsgAdapter adapter = new NewFriendsMsgAdapter(this, 1, msgs);
 		listView.setAdapter(adapter);
 		dao.saveUnreadMessageCount(0);
-		
+		initView();
 	}
 
-	public void back(View view) {
-		finish();
+	private void initView() {
+		mImgBack.setVisibility(View.VISIBLE);
+		mTxtTitle.setVisibility(View.VISIBLE);
+		mTxtTitle.setText(R.string.recommended_friends);
+		mTxtRight.setVisibility(View.VISIBLE);
+	}
+
+	@OnClick({R.id.img_back, R.id.txt_right})
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.img_back:
+				MFGT.finish(this);
+				break;
+			case R.id.txt_right:
+				MFGT.gotoAddContact(this);
+				break;
+		}
 	}
 }
